2924600e0be3c16b9ab9a245efb1046769f344f4
Mantas Jankauskas <34684969+MantasJankauskas@users.noreply.github.com>
2025-01-27 21:59:22 +0200

    small fixes

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ if __name__ == "__main__":
     model = CNN()
     model_training_statistics = TrainedModelStatistics(model=model)
     train_loader, val_loader, test_loader = get_data_for_training(num_workers=2, add_image_augmentation=True)
-    trained_model = train_model(model, model_training_statistics, train_loader, val_loader, epochs=1, show_info=True)
+    trained_model = train_model(model, model_training_statistics, train_loader, val_loader, epochs=5, show_info=True)
 
-    # test_model(trained_model, test_loader)
+    test_model(trained_model, test_loader)
 
     model_training_statistics.show_confusion_matrix(test_loader=test_loader)
     model_training_statistics.plot_training_graphs()
